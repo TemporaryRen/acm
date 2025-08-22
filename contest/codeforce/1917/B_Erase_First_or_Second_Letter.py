c91e97ba2d37d886fa9329defa5636e25191b9d8
zhaoruncheng <zhaoruncheng@patsnap.com>
2025-08-22 13:27:05 +0800

    codeforce div3 2131

---
+++ contest/codeforce/1917/B_Erase_First_or_Second_Letter.py
@@ -0,0 +1,47 @@
+packages = 1
+def I():
+    return input()
+def II():
+    return int(input())
+def MII():
+    return map(int, input().split())
+def LI():
+    return list(input().split())
+def LII():
+    return list(map(int, input().split()))
+def LFI():
+    return list(map(float, input().split()))
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+def LGMI():
+    return list(map(lambda x: int(x) - 1, input().split()))
+
+if packages:
+    from io import BytesIO, IOBase
+    import random
+    import os
+    import bisect
+    import typing
+    from collections import Counter, defaultdict, deque
+    from copy import deepcopy
+    from functools import cmp_to_key, lru_cache, reduce
+    from heapq import merge, heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+    from itertools import accumulate, combinations, permutations, count, product
+    from operator import add, iand, ior, itemgetter, mul, xor
+    from string import ascii_lowercase, ascii_uppercase, ascii_letters
+    from typing import *
+
+t = II()
+for _ in range(t):
+    n = II()
+    s = I()
+    vis = [False] * 26
+    basic = ord('a')
+    ans = 0
+    for i,x in enumerate(s):
+        p = ord(x) - basic
+        if vis[p]:
+            continue
+        ans += n - i
+        vis[p] = True 
+    print(ans)
---
+++ contest/codeforce/2131/A_Lever.py
@@ -0,0 +1,45 @@
+packages = 1
+def I():
+    return input()
+def II():
+    return int(input())
+def MII():
+    return map(int, input().split())
+def LI():
+    return list(input().split())
+def LII():
+    return list(map(int, input().split()))
+def LFI():
+    return list(map(float, input().split()))
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+def LGMI():
+    return list(map(lambda x: int(x) - 1, input().split()))
+
+if packages:
+    from io import BytesIO, IOBase
+    import random
+    import os
+    import bisect
+    import typing
+    from collections import Counter, defaultdict, deque
+    from copy import deepcopy
+    from functools import cmp_to_key, lru_cache, reduce
+    from heapq import merge, heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+    from itertools import accumulate, combinations, permutations, count, product
+    from operator import add, iand, ior, itemgetter, mul, xor
+    from string import ascii_lowercase, ascii_uppercase, ascii_letters
+    from typing import *
+
+t = II()
+for _ in range(t):
+    n = II()
+    a = LII()
+    b = LII()
+    cnt1 = 0
+    for i in range(n):
+        if a[i] > b[i]:
+            cnt1 += a[i] - b[i]
+    ans = cnt1 + 1
+    print(ans)
+    
