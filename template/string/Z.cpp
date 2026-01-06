b826929bb678cf5f62c70b3cda4d41b6344d4d46
temporary <temporary.linshi.ren@gmail.com>
2026-01-06 15:29:41 +0800

    fix z function in cpp

--- template/string/Z.cpp
+++ template/string/Z.cpp
@@ -6,7 +6,7 @@ vector<int> Z_fun(string s){
     vector Z(n,0);
     for(int j=1,l=0,r=0;j<n;j++){
         Z[j] = max(0,min(r-j+1,Z[j-l]));
-        while(j+Z[j]<n&&s[j+Z[j]]) ++Z[j];
+        while(j+Z[j]<n&&s[j+Z[j]]==s[Z[j]]) ++Z[j];
         if(j+Z[j]-1 > r){
             r = j+Z[j]-1;
             l = j;
