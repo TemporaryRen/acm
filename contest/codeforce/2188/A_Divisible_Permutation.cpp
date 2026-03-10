de63f2d6ae22db3ceb3b1fe2760bdc08a3a796d0
temporary <temporary.linshi.ren@gmail.com>
2026-03-10 10:06:45 +0800

    cf

---
+++ contest/codeforce/2188/A_Divisible_Permutation.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define forn(i, l, r) for(int i=l;i<r;i++)
+#define for0(i, r, l) for(int i=r;i>l;i--)
+#define all(a) a.begin(),a.end()
+#define forn(i, l, r) for(int i=l;i<r;i++)
+
+
+typedef long long ll;
+
+
+void solve(){
+    int n;cin>>n;
+    vector ans(n,0);
+    int l = 1, r = n;
+    int p = n - 1;
+    while(l <= r){
+        ans[p--] = l++;
+        if(l <= r) ans[p--] = r--;
+    }
+    forn(i,0,n) cout<<ans[i]<<" ";
+    cout<<"\n";
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        solve();
+    }
+    return 0;
+}
